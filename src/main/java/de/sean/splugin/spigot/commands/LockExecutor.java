2f7bce40e449b36889343f7cdf03fc43c7d934e1
pnda <43609023+ThePNDA@users.noreply.github.com>
2020-08-06 20:43:23 +0200

    /lock now defaults to /lock private

--- src/main/java/de/sean/splugin/spigot/commands/LockExecutor.java
+++ src/main/java/de/sean/splugin/spigot/commands/LockExecutor.java
@@ -1,6 +1,7 @@
 package de.sean.splugin.spigot.commands;
 
 /* SPlugin */
+
 import de.sean.splugin.App;
 import de.sean.splugin.util.SLockUtil;
 
@@ -19,46 +20,44 @@ import org.bukkit.entity.Player;
 public class LockExecutor implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (args.length > 0) {
-            Player player = Bukkit.getPlayer(sender.getName());
-            Player newPlayer;
-            if (player == null) return false;
-            switch (args[0]) {
-                case "add":
-                    if (args.length != 2) return false;
-                    newPlayer = Bukkit.getPlayer(args[1]);
-                    if (newPlayer == null) return false;
-                    SLockUtil.addUserToBeAddedFromLocking(player.getUniqueId(), newPlayer.getUniqueId());
-                    return true;
-                case "remove":
-                    if (args.length != 2) return false;
-                    newPlayer = Bukkit.getPlayer(args[1]);
-                    if (newPlayer == null) return false;
-                    SLockUtil.addUserToBeRemovedFromLocking(player.getUniqueId(), newPlayer.getUniqueId());
-                    return true;
-                case "private":
-                    SLockUtil.addUserToAddLocking(player.getUniqueId());
-                    return true;
-                case "public":
-                    SLockUtil.addUserToRemoveLocking(player.getUniqueId());
-                    return true;
-                case "info":
-                    if (!sender.isOp()) return false;
-                    SLockUtil.addUserToInfo(player.getUniqueId());
-                    return true;
-                case "clear":
-                    if (!sender.isOp()) return false;
-                    SLockUtil.addRemoveLockingForUser(player.getUniqueId());
-                    return true;
-                case "place":
-                    FileConfiguration config = App.getInstance().getConfig();
-                    config.set("Players." + player.getUniqueId() + ".NotLockOnPlace", !config.getBoolean("Players." + player.getUniqueId() + ".NotLockOnPlace"));
-                    SUtil.saveConfigFile(config);
-                    break;
-                default:
-                    // A unknown argument was passed.
-                    return false;
-            }
+        Player player = Bukkit.getPlayer(sender.getName());
+        Player newPlayer;
+        if (player == null) return false;
+        if (args.length == 0 || args[0].equals("private")) {
+            SLockUtil.addUserToAddLocking(player.getUniqueId());
+            return true;
+        }
+        switch (args[0]) {
+            case "add":
+                if (args.length != 2) return false;
+                newPlayer = Bukkit.getPlayer(args[1]);
+                if (newPlayer == null) return false;
+                SLockUtil.addUserToBeAddedFromLocking(player.getUniqueId(), newPlayer.getUniqueId());
+                return true;
+            case "remove":
+                if (args.length != 2) return false;
+                newPlayer = Bukkit.getPlayer(args[1]);
+                if (newPlayer == null) return false;
+                SLockUtil.addUserToBeRemovedFromLocking(player.getUniqueId(), newPlayer.getUniqueId());
+                return true;
+            case "public":
+                SLockUtil.addUserToRemoveLocking(player.getUniqueId());
+                return true;
+            case "info":
+                if (!sender.isOp()) return false;
+                SLockUtil.addUserToInfo(player.getUniqueId());
+                return true;
+            case "clear":
+                if (!sender.isOp()) return false;
+                SLockUtil.addRemoveLockingForUser(player.getUniqueId());
+                return true;
+            case "place":
+                FileConfiguration config = App.getInstance().getConfig();
+                config.set("Players." + player.getUniqueId() + ".NotLockOnPlace", !config.getBoolean("Players." + player.getUniqueId() + ".NotLockOnPlace"));
+                SUtil.saveConfigFile(config);
+                break;
+            default:
+                return false;
         }
         return false;
     }

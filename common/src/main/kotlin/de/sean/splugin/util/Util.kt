4d0dc334f06440ce8bee6436ba2c56d4f79b341a
pnda <43609023+ThePNDA@users.noreply.github.com>
2021-01-08 04:11:37 +0100

    Fix concatArrayRange throwing false-positive error

--- common/src/main/kotlin/de/sean/splugin/util/Util.kt
+++ common/src/main/kotlin/de/sean/splugin/util/Util.kt
@@ -5,13 +5,13 @@ object Util {
      * Concatenates a Array<String> into one long String
      * @param arr The Array to concatenate
      * @param begin The begin index inside of the given Array. Defaults to 0
-     * @param end The end index inside of the given Array. Defaults to the size of the Array minus 1
+     * @param end The end index inside of the given Array. Defaults to the size of the Array
      * @throws IllegalArgumentException if the `begin` is larger than `end` or if `end` is larger than the array's size
      */
-    fun concatArrayRange(arr: Array<String>, begin: Int = 0, end: Int = arr.size - 1): String {
+    fun concatArrayRange(arr: Array<String>, begin: Int = 0, end: Int = arr.size): String {
         if (begin < 0) throw java.lang.IllegalArgumentException("begin cannot be lower than 0")
         if (begin > end) throw IllegalArgumentException("begin cannot be larger than end")
-        if (end > arr.size - 1) throw IllegalArgumentException("end index cannot be larger than the size of the array")
+        if (end > arr.size) throw IllegalArgumentException("end index cannot be larger than the size of the array")
         val builder = StringBuilder()
         for (i in begin until end) {
             builder.append(arr[i]).append(" ")

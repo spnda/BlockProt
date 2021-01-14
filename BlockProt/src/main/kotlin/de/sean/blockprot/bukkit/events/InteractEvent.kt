27a5277637aa1cf72dbebbd7d66af4fc3912833e
pnda <43609023+ThePNDA@users.noreply.github.com>
2021-01-14 09:11:58 +0100

    Fix wrong inventory getting shown

--- BlockProt/src/main/kotlin/de/sean/blockprot/bukkit/events/InteractEvent.kt
+++ BlockProt/src/main/kotlin/de/sean/blockprot/bukkit/events/InteractEvent.kt
@@ -37,7 +37,7 @@ open class InteractEvent : Listener {
                             LockUtil.add(playerUuid, Vector3f.fromDouble(blockState.block.location.x, blockState.block.location.y, blockState.block.location.z))
                             val redstone = handler.getRedstone()
                             val inv: Inventory = BlockLockInventory.inventory
-                            if ((owner.isNotEmpty() && owner == playerUuid) || (player.isOp)) {
+                            if ((owner.isNotEmpty() && owner == playerUuid) || (owner.isNotEmpty() && player.isOp)) {
                                 inv.setItem(0, getItemStack(1, blockState.type, "Unlock"))
                                 inv.setItem(
                                     1,

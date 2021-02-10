72f80215fb0c7643be017e1d253e06c10236726c
pnda <43609023+ThePNDA@users.noreply.github.com>
2021-02-10 15:51:56 +0100

    Compare player names using Levenshtein distance

--- BlockProt/src/main/kotlin/de/sean/blockprot/bukkit/inventories/FriendSearchInventory.kt
+++ BlockProt/src/main/kotlin/de/sean/blockprot/bukkit/inventories/FriendSearchInventory.kt
@@ -4,6 +4,7 @@ import de.sean.blockprot.BlockProt
 import de.sean.blockprot.bukkit.nbt.LockUtil
 import de.sean.blockprot.util.Strings
 import net.wesjd.anvilgui.AnvilGUI
+import org.apache.commons.lang.StringUtils
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.inventory.Inventory
@@ -14,16 +15,33 @@ object FriendSearchInventory {
 
     private val playerNames = emptyMap<UUID, Inventory?>().toMutableMap()
 
+    private fun compareStrings(s1: String, s2: String): Double {
+        var longer = s1; var shorter = s2
+        if (s1.length < s2.length) {
+            longer = s2; shorter = s1
+        }
+        val longerLength = longer.length
+        return if (longerLength == 0) 1.0 // They match 100% if both Strings are empty
+        else (longerLength - StringUtils.getLevenshteinDistance(longer, shorter)) / longerLength.toDouble()
+    }
+
     fun openAnvilInventory(requestingPlayer: Player) {
-        val gui = AnvilGUI.Builder()
+        AnvilGUI.Builder()
             // .itemRight(ItemUtil.getItemStack(1, Material.PLAYER_HEAD, null, null))
             .onComplete { player: Player, text: String ->
                 var players = Bukkit.getOfflinePlayers().toList()
 
                 // Search for players matching
                 players = players.filter {
-                    if (it.name == null) false
-                    else it.name!!.contains(text) || it.name.equals(text)
+                    // Filter all the players by search criteria.
+                    // If the strings are similar by 30%, the strings are considered similar (imo) and should be added.
+                    // If they're less than 30% similar, we should still check if it possibly contains the search criteria
+                    // and still add that user.
+                    when {
+                        it.name == null -> false
+                        compareStrings(it.name!!, text) > 0.3 -> true
+                        else -> it.name!!.contains(text, ignoreCase = true)
+                    }
                 }
 
                 playerNames[player.uniqueId] = FriendSearchResultInventory.createInventoryAndFill(player, players)

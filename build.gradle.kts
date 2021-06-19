ea130a3738e2d3d7d7ab649352b1820560d919f2
spnda <43609023+spnda@users.noreply.github.com>
2021-06-19 22:05:39 +0200

    Bump version to 0.3.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ fun readEditorConfigRules(): Map<String, String> {
 }
 
 group = "de.sean"
-version = "0.3.1"
+version = "0.3.2"
 base.archivesName.set("${project.name}-$version-${gitBranchName()}")
 
 repositories {

01c64602e0999ee1f2ea1d078fb92562d067ef2f
sean <43609023+spnda@users.noreply.github.com>
2021-12-08 14:57:41 +0100

    Fix 13081a8: Check interaction with doors and other blocks before opening the inventory as well

--- spigot/src/main/java/de/sean/blockprot/bukkit/listeners/InteractEventListener.java
+++ spigot/src/main/java/de/sean/blockprot/bukkit/listeners/InteractEventListener.java
@@ -22,9 +22,13 @@ import de.sean.blockprot.bukkit.BlockProt;
 import de.sean.blockprot.bukkit.BlockProtAPI;
 import de.sean.blockprot.bukkit.TranslationKey;
 import de.sean.blockprot.bukkit.Translator;
+import de.sean.blockprot.bukkit.events.BlockAccessEvent;
+import de.sean.blockprot.bukkit.nbt.BlockNBTHandler;
 import de.sean.blockprot.bukkit.nbt.NBTHandler;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.block.data.type.Door;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -41,7 +45,20 @@ public class InteractEventListener implements Listener {
         if (BlockProt.getDefaultConfig().isWorldExcluded(event.getClickedBlock().getWorld())) return;
         if (!BlockProt.getDefaultConfig().isLockable(event.getClickedBlock().getState().getType())) return;
         Player player = event.getPlayer();
-        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && player.isSneaking() && player.hasPermission(NBTHandler.PERMISSION_LOCK)) {
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && !player.isSneaking()) {
+            BlockAccessEvent accessEvent = new BlockAccessEvent(event.getClickedBlock(), player);
+            Bukkit.getPluginManager().callEvent(accessEvent);
+            if (accessEvent.isCancelled()) {
+                event.setCancelled(true);
+                sendMessage(player, Translator.get(TranslationKey.MESSAGES__NO_PERMISSION));
+            } else {
+                BlockNBTHandler handler = new BlockNBTHandler(event.getClickedBlock());
+                if (!(handler.canAccess(player.getUniqueId().toString()) || player.hasPermission(NBTHandler.PERMISSION_BYPASS))) {
+                    event.setCancelled(true);
+                    sendMessage(player, Translator.get(TranslationKey.MESSAGES__NO_PERMISSION));
+                }
+            }
+        } else if (event.getAction() == Action.RIGHT_CLICK_BLOCK && player.isSneaking() && player.hasPermission(NBTHandler.PERMISSION_LOCK)) {
             if (event.hasItem()) return; // Only enter the menu with an empty hand.
 
             event.setCancelled(true);
